8e700d03e9470a75706ecdd855cb8b7bcfe46e70
alihaidar2950 <alihaidar2950@gmail.com>
2024-01-05 17:53:50 -0500

    Adding player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Player from './components/Player';
+
 function App() {
   return (
-    <header>
-      <img src="" alt="" />
-      <h1>Tic-Tac-Toe</h1>
-    </header>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+      </div>
+      LOG
+    </main>
   );
 }
 
